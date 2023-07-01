81e4ec686a6836835c779553ffd881c9190e8767
Shadow15510 <antoine.royer14@orange.fr>
2023-07-01 10:35:57 +0200

    Second try of auto-generated doc

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+sys.path.append(os.path.abspath('../../src'))
+
 project = 'IRC API'
 copyright = '2023, Sha-chan'
 author = 'Sha-chan'
