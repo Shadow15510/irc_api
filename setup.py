15d21a9d606cc5a51f28e88c6e8ac0c31a4b638b
Shadow15510 <antoine.royer14@orange.fr>
2023-07-21 11:21:57 +0200

    Finishing tests

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
 from setuptools import setup
 
 if __name__ == "__main__":
-    with open("requirements.txt", "r", encoding="utf-8") as file:
-        requirements = [line.strip("\n") for line in file.readlines()]
-    setup(install_requires=requirements)
+    setup()
