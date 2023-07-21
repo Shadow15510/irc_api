369e6aaec2d936793e779c702c27387b350dd198
Shadow15510 <antoine.royer14@orange.fr>
2023-07-21 11:16:36 +0200

    Update setup

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
 
 if __name__ == "__main__":
-    setup()
+    with open("requirements.txt", "r", encoding="utf-8") as file:
+        requirements = [line.strip("\n") for line in file.readlines()]
+    setup(install_requires=requirements)
